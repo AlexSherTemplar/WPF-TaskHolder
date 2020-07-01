d573dfeab07f3cfc2f0a4a4d7788cb16b4af9474
tduk-tik <tduk-tik@mail.ru>
2020-07-01 17:46:03 +0300

    wrote the basis of the organizer. 4 columns (easy to scale), save and load system

---
+++ Models/TaskModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    class TaskModel : INotifyPropertyChanged
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        private bool isDone;
+        private string text;
+        private string comment;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsDone
+        {
+            get { return isDone; }
+            set
+            {
+                if (isDone == value)
+                {
+                    return;
+                }
+                isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                if (text == value)
+                    return;
+                text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+        public string Comment
+        {
+            get { return comment; }
+            set
+            {
+                if (comment == value)
+                    return;
+                comment = value;
+                OnPropertyChanged("Comment");
+            }
+        }
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+    }
+}
---
+++ Services/FileIOService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp.Services
+{
+    class FileIOService
+    {
+        private string PATH;
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+        public BindingList<TaskModel> LoadData()
+        {
+            var fileExist = File.Exists(PATH);
+            if (!fileExist)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<TaskModel>();
+            }
+            
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<TaskModel>>(fileText);
+            }
+
+            
+        }
+        public void SaveData(object taskDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(taskDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
